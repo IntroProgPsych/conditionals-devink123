845285e1a76cf0d566f52e7fbc8f5c8a522230db
devink123 <amira.kordi06@e-uvt.ro>
2025-11-20 10:10:25 +0000

    didn't manage to finish this:(

--- Part2_Copilot/5_Sintax.py
+++ Part2_Copilot/5_Sintax.py
@@ -18,7 +18,7 @@
 number = input("Please type in a number: ")
 if number>100:
     print("The number was greater than one hundred")
-    number - 100
+elif number ==number-100:
     print("Now its value has decreased by one hundred")
     print("Its value is now"+ number)
 print(number + "must be my lucky number!")
